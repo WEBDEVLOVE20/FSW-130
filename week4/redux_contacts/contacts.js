07e64b77c7971005f63eb55e4fcbb865bf3e138f
Kelly Compton <kellycompton@Kellys-MacBook-Pro.local>
2020-08-23 22:24:45 -0400

    #

--- week4/redux_contacts/contacts.js
+++ week4/redux_contacts/contacts.js
@@ -56,11 +56,12 @@ store.subscribe(() => {
     console.log(store.getState())
 })
 
+// ADD CONTACT
 store.dispatch(addNewContact({name: "Jim Thorton", phone: "456-234-2303", email: "superjim@gsnail.com"}))
- store.dispatch(addNewContact({name: "Emily Bay", phone: "456-434-3822", email: "baybay@gsnail.com"}))
-// store.dispatch(addNewContact({name: "Jamie Casel", phone: "717-334-2343", email: "jamiec@gsnail.com"}))
-// store.dispatch(addNewContact({name: "Leah Smith", phone: "234-234-2343", email: "smithyl@gsnail.com"}))
+store.dispatch(addNewContact({name: "Emily Bay", phone: "456-434-3822", email: "baybay@gsnail.com"}))
+store.dispatch(addNewContact({name: "Jamie Casel", phone: "717-334-2343", email: "jamiec@gsnail.com"}))
+store.dispatch(addNewContact({name: "Leah Smith", phone: "234-234-2343", email: "smithyl@gsnail.com"}))
 
-
+// DELETE CONTACT
 store.dispatch(removeContact({name: "Jim Thorton", phone: "456-234-2303", email: "superjim@gsnail.com"}))
-// store.dispatch(removeContact({name: "Jamie Casel", phone: "717-334-2343", email: "jamiec@gsnail.com"}))
+store.dispatch(removeContact({name: "Jamie Casel", phone: "717-334-2343", email: "jamiec@gsnail.com"}))
