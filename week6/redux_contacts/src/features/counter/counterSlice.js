1429c8455e3cf2e92dfb09aefc50198a159ed7ce
Kelly Compton <kellycompton@Kellys-MacBook-Pro.local>
2020-09-05 23:03:15 -0400

    #

---
+++ week6/redux_contacts/src/features/counter/counterSlice.js
@@ -0,0 +1,38 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+export const counterSlice = createSlice({
+  name: 'counter',
+  initialState: {
+    users: []
+  },
+  reducers: {
+    addName: (state, action) => {
+      state.users = [...state.users, action.payload];
+    },
+    addPhone: (state, action) => {
+      state.users += action.payload;
+    },
+    addEmail: (state, action) => {
+      state.users += action.payload;
+    },
+  },
+});
+
+export const { addName, addPhone, addEmail } = counterSlice.actions;
+
+// The function below is called a thunk and allows us to perform async logic. It
+// can be dispatched like a regular action: `dispatch(incrementAsync(10))`. This
+// will call the thunk with the `dispatch` function as the first argument. Async
+// code can then be executed and other actions can be dispatched
+// export const incrementAsync = amount => dispatch => {
+//   setTimeout(() => {
+//     dispatch(incrementByAmount(amount));
+//   }, 1000);
+// };
+
+// The function below is called a selector and allows us to select a value from
+// the state. Selectors can also be defined inline where they're used instead of
+// in the slice file. For example: `useSelector((state) => state.counter.value)`
+export const selectUsers = state => state.counter.users;
+
+export default counterSlice.reducer;
